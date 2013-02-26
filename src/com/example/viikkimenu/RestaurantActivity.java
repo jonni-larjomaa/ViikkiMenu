a28b2b26b4a2ba16b1ed6beb7ad7c899c76bfd7a
Jonni <mariox@mbnet.fi>
2013-02-26 21:32:11 +0200

    + update RestaurantActivity

--- src/com/example/viikkimenu/RestaurantActivity.java
+++ src/com/example/viikkimenu/RestaurantActivity.java
@@ -1,8 +1,10 @@
 package com.example.viikkimenu;
 
 import android.os.*;
+import android.annotation.SuppressLint;
 import android.app.*;
 import android.content.*;
+import android.content.res.TypedArray;
 import android.view.View;
 import android.widget.*;
 
@@ -10,17 +12,38 @@ public class RestaurantActivity extends ListActivity {
 
 	
 	private String[] rests;
+	private TypedArray logos;
 	
-    @Override
+	@SuppressLint("Recycle")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant);
         
         // get the restaurants and names.
         rests = this.getResources().getStringArray(R.array.restaurants);
-        setRestaurantListView(rests);
+        logos = this.getResources().obtainTypedArray(R.array.restlogos);
         
+        
+        // create adapter
+        RestaurantArrayAdapter restsAA = 
+        		new RestaurantArrayAdapter(this, 
+        								   R.layout.restaurantlistitem,
+        								   rests,
+        								   logos);
+        
+        // set list adapter for restaurants.
+        setListAdapter(restsAA);
     }
+	
+	
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// when destroyed clean up
+		logos.recycle();
+	}
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -28,19 +51,6 @@ public class RestaurantActivity extends ListActivity {
 		i.putExtra("restaurant", rests[position]);
 		i.putExtra("position", position);
 		startActivity(i);
-	}
-	
-	// convinience method for setting up list view for activity.
-	private void setRestaurantListView(String[] r){
-		
-		// create adapter
-        ArrayAdapter<String> restsAA = new ArrayAdapter<String>(this, 
-        														R.layout.restaurantlistitem,
-        														R.id.srestaurant,
-        														r);
-        // set list adapter for restaurants.
-        this.setListAdapter(restsAA);
-	}
-    
+	}    
     
 }
